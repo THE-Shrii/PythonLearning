9edcc099c68825ef7f3f28f15455c420d26aac7a
Shristi Kumari <shristikumari1910@gmail.com>
2026-05-29 17:40:43 +0530

    Operator test

---
+++ operatortest.py
@@ -0,0 +1,105 @@
+# Add two numbers using arithmetic operator.
+# a = 10
+# b = 23
+# print(a+b)
+
+# Find remainder when 17 is divided by 5.
+# print(17%5)
+
+# Find floor division of 17 and 5.
+# print(17 // 5)
+
+
+# Find value of 2 raised to power 4.
+# print(2**4)
+
+# Add and Assign
+# x = 10
+# x += 5
+# print(x)
+
+#Multiply and assign
+# a =a = 6
+# a *= 3
+# print(a)
+
+# Use %= operator.
+# num = 20
+# num %= 3
+# print(num)
+
+
+# Check whether two numbers are equal.
+# a = 10
+# b = 10
+# print(a == b)
+
+# Check if 15 is greater than 8.
+# print(15 > 8)
+
+
+
+# Compare two strings.
+# print("apple" == "apple")
+# print("apple" > "banana")
+
+
+
+# Check if both conditions are true.
+# age = 20
+# has_id = True
+# print(age >= 18 and has_id)
+
+
+# OR Operator
+# Check if at least one condition is true.
+#
+# a = 5
+# b = 12
+# print(a > 10 or b > 10)
+
+
+# Reverse boolean value.
+# is_logged_in = False
+# print(not is_logged_in)
+
+
+# Swap Two Numbers Without Third Variable
+# a = 5
+# b = 10
+
+
+# Method 1 → Using Python Multiple Assignment (Best Method)
+# Code
+# a = 5
+# b = 10
+#
+# a, b = b, a
+#
+# print("a =", a)
+# print("b =", b)
+
+
+# Method 2 → Using Addition and Subtraction
+# Code
+# a = 5
+# b = 10
+#
+# a = a + b
+# b = a - b
+# a = a - b
+#
+# print(a)
+# print(b)
+
+# Method 3 → Using Multiplication and Division
+# Code
+# a = 5
+# b = 10
+#
+# a = a * b
+# b = a / b
+# a = a / b
+#
+# print(a)
+# print(b)
