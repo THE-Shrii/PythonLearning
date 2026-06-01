f34b9ad67f90443fc74714b4e6c629232c11999d
Shristi Kumari <shristikumari1910@gmail.com>
2026-06-01 11:05:56 +0530

    If else test

---
+++ if_elsee_test.py
@@ -0,0 +1,182 @@
+# 1. Check whether a number is positive, negative, or zero.
+
+# num = int(input("Enter a number: "))
+# if num > 0:
+#     print(num, "is positive")
+# elif num == 0:
+#     print(num, "is zero")
+# else:
+#     print(num, "is negative")
+
+
+
+
+# 2. Check whether a number is even or odd.
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# 3. A person can vote if age is 18 or above.
+
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("Not eligible to vote")
+
+
+
+# 4. Check Greatest of Two Numbers
+
+# a = int(input("Enter 1st number:"))
+# b = int(input("Enter 2nd number: "))
+# if a>b:
+#     print(f"{a} is greater than {b}")
+# else:
+#     print(f"{a} is less than {b}")
+
+#5.Greatest of Three Numbers
+
+# a = 122
+# b = 22
+# c = 3
+# if a>b and a>c:
+#     print("a is greater")
+# elif b>a and b>c:
+#     print("b is greater")
+# else:
+#     print("c is greater")
+
+
+
+# 6.Check whether a number is divisible by both 5 and 11.
+
+# num = int(input("Enter a number: "))
+# if num % 5 == 0 and num % 11 == 0 :
+#     print("The number is divisible ")
+# else:
+#     print("The number is not divisible ")
+
+
+
+# 7.Check whether a year is a leap year.
+
+# year = int(input("Enter a year: "))
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("Its leap year")
+# else:
+#     print("Not leap year")
+
+# 8.Pass if marks are 40 or above.
+
+# marks = int(input("Enter marks: "))
+# if marks >= 100 or marks >= 75:
+#     print("Grade A")
+# elif marks >= 50 or marks >= 25:
+#     print("Grade B")
+# else:
+#     print("Fail")
+
+
+
+
+
+# 10. Largest Among Four Numbers
+
+# a = 8
+# b = 4
+# c = 3
+# d = 5
+#
+# if a>b and a>c and a>d:
+#     print("A is boss")
+# elif b>a and b>c and b>d:
+#     print("B is boss")
+# elif c>a and c>b and c>d:
+#     print("C is boss")
+# elif d>a and d>b and d>c:
+#     print("D is boss")
+
+
+
+
+
+
+# Check Character is Vowel or Consonant
+
+# char = 'q'
+# if char.lower() in "aeiou":
+#     print('yes')
+# else:
+#     print('no')
+
+
+# 12. Find Absolute Value
+# num = -15
+#
+# if num < 0:
+#     num = -num
+#
+# print(num)
+
+
+
+
+
+# 13. Login System
+# username = "admin"
+# password = "1234"
+#
+# if username == "admin" and password == "1234":
+#     print("Login Successful")
+# else:
+#     print("Invalid Credentials")
+
+
+
+
+# 14.Check whether three sides can form a triangle.
+# a = 4
+# b = 7
+# c = 9
+# if a+b>c and a+c>b and b+c>a:
+#     print('Valid triangle')
+# else:
+#     print('Invalid triangle')
+
+
+
+# 15. Profit or Loss
+# sp = 200
+# cp = 200
+# if sp > cp:
+#     print("Profit")
+# elif cp > sp:
+#     print("Loss")
+# else:
+#     print("No loss no profit")
+
+
+# 16. Electricity Bill Category
+# units = 250
+#
+# if units <= 100:
+#     print("Low Usage")
+# elif units <= 300:
+#     print("Medium Usage")
+# else:
+#     print("High Usage")
+
+
+# 17. Check Number is Three-Digit/
+# num = 3900
+# if 100 <= num <= 999:
+#     print("3 digit number")
+# else:
+#     print("Not 3 digit number")
+
+
