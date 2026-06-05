3b4c3829c7ebfe75681ef3f474c7e1cf1154cc1c
Shristi Kumari <shristikumari1910@gmail.com>
2026-06-05 11:18:48 +0530

    Oops  test1

---
+++ oops_test.py
@@ -0,0 +1,54 @@
+# ----------------------------------------------------------
+# 1. Create a Class and Object
+# ----------------------------------------------------------
+# class Shrii:
+#     pass
+# s1 = Shrii()
+# print(type(s1))
+
+
+# ----------------------------------------------------------
+# 2. Constructor Example
+# ----------------------------------------------------------
+# class A:
+#     def __init__(self):
+#         print("Constructor called")
+# s = A()
+
+
+
+# ----------------------------------------------------------
+# 3. Constructor with Parameters
+# ----------------------------------------------------------
+# class B:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# s = B("Shrii",21)
+# print(s.name)
+# print(s.age)
+
+
+
+
+# ----------------------------------------------------------
+# 4. Instance Method
+# ----------------------------------------------------------
+# class Student:
+#     def show(self): #INSTANCE METHOD
+#         print("Hello")
+# s = Student()
+# s.show()
+
+
+
+# ----------------------------------------------------------
+# 5. Multiple Objects
+# ----------------------------------------------------------
+# class A:
+#     def __init__(self,name):
+#         self.name = name
+# s1 = A('Shrii')
+# s2 = A('Shreya')
+# print(s1.name)
+# print(s2.name)
